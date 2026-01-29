69cd5d71b6245e09dbcffe3ac55516f70b9cc174
Ambayo Saviour Luga <ambayosaviourluga@gmail.com>
2026-01-29 20:20:23 +0300

    Added dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
     res.send("API is Running ................");
 });
 
-app.listen(port, ()=> {console.log(`Server Running on Port ${port}.`)});
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}.`)
+});
 
 
