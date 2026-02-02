bc70015568dbadd12d4aafc5e3d371f7cfa2f2fa
Ambayo Saviour Luga <ambayosaviourluga@gmail.com>
2026-02-02 13:10:48 +0300

    Edited the package.json file

--- Backend/server.js
+++ Backend/server.js
@@ -4,22 +4,22 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-const __dirname = path.resolve();
+//const __dirname = path.resolve();
 
-app.use("/upload", express.static(path.join(__dirname, "/upload")));
+//app.use("/upload", express.static(path.join(__dirname, "/upload")));
 
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+//if (process.env.NODE_ENV === "production") {
+//  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    app.get("*", (rqs, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", index.html));
-    });
-}else{
+//   app.get("*", (rqs, res) => {
+//       res.sendFile(path.resolve(__dirname, "frontend", "build", index.html));
+//   });
+//}else{
     app.get("/", (req, res) => {
         res.send("API is Running ................");
     });
-}
+//}
 
 app.listen(port, () => {
     console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${port}.`)
