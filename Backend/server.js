baa9fad6d72b5714f4cf4bc0a5e027a6bebe66cf
Ambayo Saviour Luga <ambayosaviourluga@gmail.com>
2026-02-02 12:26:37 +0300

    Fixed the deployment bug

--- Backend/server.js
+++ Backend/server.js
@@ -4,12 +4,25 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("API is Running ................");
-});
+const __dirname = path.resolve();
+
+app.use("/upload", express.static(path.join(__dirname, "/upload")));
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    app.get("*", (rqs, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", index.html));
+    });
+}else{
+    app.get("/", (req, res) => {
+        res.send("API is Running ................");
+    });
+}
 
 app.listen(port, () => {
-    console.log(`Server Running on Port ${port}.`)
+    console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${port}.`)
 });
 
 
